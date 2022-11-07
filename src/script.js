7ab36ec71af17d090602e309d7aa742c07fc77bd
jucasoliveira <jucas.oliveira@gmail.com>
2022-11-07 18:36:55 -0300

    clean

--- src/script.js
+++ src/script.js
@@ -7,8 +7,6 @@ function move() {
     // if elm style width is 0, set it to 100% else get next element and set it to 100%
     var width = 0;
     var id = setInterval(frame, 10);
-    // keep the previous bar value
-    var prev = 0;
     function frame() {
       if (width >= 100) {
         clearInterval(id);
@@ -21,8 +19,6 @@ function move() {
       } else {
         width++;
         elem[elem.length - 1].style.width = width + "%";
-        // update the previous bar value
-        prev = width;
       }
     }
   }
